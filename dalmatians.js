17dd27a6add145caa309b386944209c55e4bf07e
Sofia Krsmanovic <38912502+sofiakrsmanovic@users.noreply.github.com>
2018-06-25 00:19:28 -0500

    javascript

---
+++ dalmatians.js
@@ -0,0 +1,9 @@
+var numberOfDalmatians = 2;
+
+while (numberOfDalmatians < 100)
+{
+	console.log ("There are " + numberOfDalmatians + " dalmatians safe! Time to rescue another one!");
+	numberOfDalmatians++;
+}
+
+console.log ("Hooray! All " + numberOfDalmatians + " dalmatians are safe! Roll the credits!");
